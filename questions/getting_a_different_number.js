b7a39db0182da44453f82e9f4dcdcc2e7aea3329
liya <jingsongroberge114@gmail.com>
2024-04-25 20:53:52 +0800

    Create getting_a_different_number.js

---
+++ questions/getting_a_different_number.js
@@ -0,0 +1,11 @@
+/**
+ * Getting a Different Number
+
+Given an array arr of unique nonnegative integers, implement a function getDifferentNumber that finds the smallest nonnegative integer that is NOT in the array.
+
+input:  arr = [0, 1, 2, 3]
+output: 4 
+
+input:  arr = [0, 2, 3]
+output: 1
+ */
