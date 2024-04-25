182c652ad8e05a625178cffa94fc00633cbd8b04
liya <jingsongroberge114@gmail.com>
2024-04-25 20:53:19 +0800

    Create flatten_a_dictionary.js

---
+++ questions/flatten_a_dictionary.js
@@ -0,0 +1,29 @@
+/**
+ * Flatten a Dictionary
+
+Given a dictionary dict, write a function flattenDictionary that returns a flattened version of it.
+If a certain key is empty, it should be excluded from the output (see e in the example below).
+
+
+input:  dict = {
+            "Key1" : "1",
+            "Key2" : {
+                "a" : "2",
+                "b" : "3",
+                "c" : {
+                    "d" : "3",
+                    "e" : {
+                        "" : "1"
+                    }
+                }
+            }
+        }
+
+output: {
+            "Key1" : "1",
+            "Key2.a" : "2",
+            "Key2.b" : "3",
+            "Key2.c.d" : "3",
+            "Key2.c.e" : "1"
+        }
+ */
