b9eae3281e811d357f2c95ff99371e372496bf4d
liya <jingsongroberge114@gmail.com>
2024-04-26 15:29:37 +0800

    Create smallest_substr_all_chars.js

---
+++ questions/smallest_substr_all_chars.js
@@ -0,0 +1,11 @@
+/**
+ * Smallest Substring of All Characters (Minimum Window Substring)
+
+Given an array of unique characters arr and a string str, Implement a function getShortestUniqueSubstring that finds the smallest substring of str 
+containing all the characters in arr. Return "" (empty string) if such a substring doesn’t exist.
+
+input:  arr = ['x','y','z'], str = "xyyzyzyx"
+
+output: "zyx"
+
+ */
