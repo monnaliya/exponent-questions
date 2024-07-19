7d0ae9aa4ef77500d4e15537b198d9b6596e6836
huigeek <463027965@qq.com>
2024-07-19 12:45:34 +0800

    Update island_count.js

--- questions/island_count.js
+++ questions/island_count.js
@@ -16,4 +16,47 @@ input:  binaryMatrix = [ [0,    1,    0,    1,    0],
 
 output: 6 # since this is the number of islands in binaryMatrix.
           # See all 6 islands color-coded below.
- */
+*/
+
+function getNumberOfIslands(binaryMatrix) {
+    const ROWLEN = binaryMatrix.length;
+    const COLLEN = binaryMatrix[0].length;
+    let counts = 0;
+
+    for(let i = 0; i < ROWLEN; i++) {
+        for (let j = 0; j < COLLEN; j++) {
+            // current cell is binaryMatrix[i][j]
+            if (binaryMatrix[i][j] === 1) {
+                counts++;
+                dfs(binaryMatrix, i, j);
+            }
+        }
+    }
+
+    function dfs(grid, row, col) {
+        if (row < 0 || col < 0 || row >= ROWLEN || col >= COLLEN || grid[row][col] === 0) {
+            return;
+        }
+
+        // mark cell 0 as visited
+        grid[row][col] = 0;
+
+        // traverse all 4 directions
+        dfs(grid, row - 1, col); // up
+        dfs(grid, row + 1, col); // down
+        dfs(grid, row, col - 1); // left
+        dfs(grid, row, col + 1); // right
+    }
+
+    return counts;
+}
+
+binaryMatrix = [ 
+    [0,    1,    0,    1,    0],
+    [0,    0,    1,    1,    1],
+    [1,    0,    0,    1,    0],
+    [0,    1,    1,    0,    0],
+    [1,    0,    1,    0,    1] ];
+
+console.log(getNumberOfIslands(binaryMatrix))
+
