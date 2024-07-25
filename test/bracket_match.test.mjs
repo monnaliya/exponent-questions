4778bbd7c82867c4046f96ad2c48c86e58889ef4
liya <jingsongroberge114@gmail.com>
2024-07-26 01:45:50 +0800

    Create bracket_match.test.mjs

---
+++ test/bracket_match.test.mjs
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+import { bracketMatch1, bracketMatch2 } from '../questions/bracket_match.js';
+
+describe('bracketMatch1', () => {
+  it('should return 0 for correctly matched brackets', () => {
+    expect(bracketMatch1('')).to.equal(0);
+    expect(bracketMatch1('()')).to.equal(0);
+    expect(bracketMatch1('()()')).to.equal(0);
+    expect(bracketMatch1('(())')).to.equal(0);
+  });
+
+  it('should return the number of brackets needed to make it correctly matched', () => {
+    expect(bracketMatch1('(')).to.equal(1);
+    expect(bracketMatch1(')')).to.equal(1);
+    expect(bracketMatch1('(()')).to.equal(1);
+    expect(bracketMatch1('())')).to.equal(1);
+    expect(bracketMatch1(')(')).to.equal(2);
+    expect(bracketMatch1('()(')).to.equal(1);
+  });
+});
+
+describe('bracketMatch2', () => {
+  it('should return 0 for correctly matched brackets', () => {
+    expect(bracketMatch2('')).to.equal(0);
+    expect(bracketMatch2('()')).to.equal(0);
+    expect(bracketMatch2('()()')).to.equal(0);
+    expect(bracketMatch2('(())')).to.equal(0);
+  });
+
+  it('should return the number of brackets needed to make it correctly matched', () => {
+    expect(bracketMatch2('(')).to.equal(1);
+    expect(bracketMatch2(')')).to.equal(1);
+    expect(bracketMatch2('(()')).to.equal(1);
+    expect(bracketMatch2('())')).to.equal(1);
+    expect(bracketMatch2(')(')).to.equal(2);
+    expect(bracketMatch2('()(')).to.equal(1);
+  });
+});
