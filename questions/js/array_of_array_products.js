12c1049becf4584a375e502f680304ee7e152ae5
liya <jingsongroberge114@gmail.com>
2024-08-12 19:12:06 +0800

    Update array_of_array_products.js

--- questions/js/array_of_array_products.js
+++ questions/js/array_of_array_products.js
@@ -12,3 +12,33 @@ input:  arr = [2, 7, 3, 4]
 output: [84, 24, 56, 42] # by calculating: [7*3*4, 2*3*4, 2*7*4, 2*7*3]
 
  */
+
+export function arrayOfArrayProducts(arr) {
+    const len = arr.length;
+
+    if (len <=1 ) return [];
+
+    let result = new Array(len).fill(1);
+    let leftProducts = new Array(len).fill(1);
+    let rightProducts = new Array(len).fill(1);
+
+    for (let i = 0; i < len; i++) {
+        leftProducts[i] = i > 0 ? arr[i] * leftProducts[i - 1] : arr[i];
+    }
+
+    for (let i = len - 1; i >= 0; i--) {
+        rightProducts[i] = i < len - 1 ? arr[i] * rightProducts[i + 1] : arr[i];
+    }
+
+    for (let i = 0; i < len; i++) {
+        if (i === 0) {
+            result[i] = rightProducts[i+1];
+        } else if (i === len - 1) {
+            result[i] = leftProducts[i-1];
+        } else {
+            result[i] = leftProducts[i-1] * rightProducts[i+1];
+        }
+    }
+
+    return result;
+}
