64e8cdcb63db97f794184c2b31d2fe8800cb305b
liya <jingsongroberge114@gmail.com>
2024-08-21 18:11:35 +0800

    Update merging_2_packages.js

--- questions/js/merging_2_packages.js
+++ questions/js/merging_2_packages.js
@@ -12,3 +12,28 @@ input:  arr = [4, 6, 10, 15, 16],  lim = 21
 output: [3, 1] # since these are the indices of the
                # weights 6 and 15 whose sum equals to 21
  */
+
+
+export function getIndicesOfItemWeights1(arr, limit) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        for (let j = i - 1 ; j >= 0; j--) {
+            if (arr[i] + arr[j] === limit) {
+                return [i, j];
+            }
+        }
+    }
+    return [];
+}
+
+export function getIndicesOfItemWeights2(arr, limit) {
+    const map = new Map();
+    for (let i = 0; i < arr.length; i++) {
+        const complement = limit - arr[i];
+        if (map.has(complement)) {
+            return [i, map.get(complement)];
+        }
+        map.set(arr[i], i);
+    }
+    return [];
+}
+
