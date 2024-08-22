f4d3abb14e55a5ccbccd46198a1eccd5deb6d28e
liya <jingsongroberge114@gmail.com>
2024-08-22 09:12:12 +0800

    Update shifted_array_search.js

--- questions/js/shifted_array_search.js
+++ questions/js/shifted_array_search.js
@@ -17,3 +17,32 @@ input:  shiftArr = [9, 12, 17, 2, 4, 5], num = 2 # shiftArr is the
 
 output: 3 # since it’s the index of 2 in arr
  */
+
+export function shiftedArrSearch(shiftArr, num) {
+    let left = 0;
+    let right = shiftArr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (shiftArr[mid] === num) {
+            return mid;
+        }
+
+        // if the left half is sorted
+        if (shiftArr[left] <= shiftArr[mid]) {
+            if (num >= shiftArr[left] && num < shiftArr[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            // if the right half is sorted
+            if (num > shiftArr[mid] && num <= shiftArr[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+}   
