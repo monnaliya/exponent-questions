5f70e59f1ad83117c836b26ff0262a7cfa0fb790
liya <jingsongroberge114@gmail.com>
2024-09-27 12:54:51 +0800

    Create prime_numbers.js

---
+++ questions/js/prime_numbers.js
@@ -0,0 +1,10 @@
+/**
+Prime Numbers
+Write a function find_primes(n) that returns all prime numbers less than or equal to n.
+
+Examples
+Python
+find_primes(1)  # => []
+find_primes(5)  # => [2, 3, 5]
+find_primes(20) # => [2, 3, 5, 7, 11, 13, 17, 19]
+ */
