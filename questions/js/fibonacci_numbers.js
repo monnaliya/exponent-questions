f38440ec51ef13c66c6a5716e8f4d4a950d33ae1
liya <jingsongroberge114@gmail.com>
2024-10-11 20:52:29 +0800

    Create fibonacci_numbers.js

---
+++ questions/js/fibonacci_numbers.js
@@ -0,0 +1,16 @@
+/**
+Fibonacci Numbers
+Write a function fib(n) that returns the nth Fibonacci number. The Fibonacci sequence is defined as F(n) = F(n-1) + F(n-2) with F(0) = 1 and F(1) = 1, which produces the following pattern:
+
+1, 1, 2, 3, 5, 8, 13 ...
+Examples
+Python
+fib(0)  # => 1
+fib(1)  # => 1
+fib(2)  # => 2
+fib(3)  # => 3
+...
+fib(10) # => 89
+fib(20) # => 10946
+ */
+
