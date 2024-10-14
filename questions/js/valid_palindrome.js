ddb66eb34eb9092cc7d56eb443e3df370fc7b04d
liya <jingsongroberge114@gmail.com>
2024-10-14 21:47:25 +0700

    Create valid_palindrome.js

---
+++ questions/js/valid_palindrome.js
@@ -0,0 +1,13 @@
+/**
+Given a string s, return true if it is a palindrome, and false otherwise. A string is considered a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Write a function isPalindrome that implements this logic.
+
+Examples
+Input: s = "H123!@321h"
+Output: true
+
+Input: s = "race a car"
+Output: false
+Constraints
+1 <= s.length <= 2 * 10^5
+The input string s can include any printable ASCII characters.
+ */
