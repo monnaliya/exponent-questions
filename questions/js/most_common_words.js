fa664032fb82f066c4a9db9ec470ae07d4ab3271
liya <jingsongroberge114@gmail.com>
2024-10-14 20:53:55 +0700

    Create most_common_words.js

---
+++ questions/js/most_common_words.js
@@ -0,0 +1,29 @@
+/**
+We want to find the most frequently used words in a long string of text. Write a function most_common_words(text) that returns an array containing words with their frequencies, sorted from most common to least common, with ties broken by alphabetic order.
+
+For greater accuracy, your solution should ignore punctuation and capitalization.
+
+Python
+def most_common_words(text):
+  # Your code here
+Examples
+For example, given the string
+
+Python
+text = 'It was the best of times, it was the worst of times.'
+then most_common_words(text) should return
+
+
+JavaScript
+JavaScript
+
+[
+    ['it', 2],
+    ['of', 2],
+    ['the', 2],
+    ['times', 2],
+    ['was', 2],
+    ['best', 1],
+    ['worst', 1],
+]
+ */
