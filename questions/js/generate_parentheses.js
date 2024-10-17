c05b13f79c8f9e8752b79ecf2a3ea5f90528947e
liya <jingsongroberge114@gmail.com>
2024-10-17 19:54:24 +0700

    Create generate_parentheses.js

---
+++ questions/js/generate_parentheses.js
@@ -0,0 +1,21 @@
+/**
+Generate Parentheses
+Generate all combinations of well-formed parentheses. A parenthesis combination is well-formed if each opening parenthesis "(" is closed by a matching closing parenthesis ")" in the correct order.
+
+For example, "()", "()()", and "((()))" are all combinations of well-formed parentheses, while ")(", "((" and "(()" are not.
+
+Examples:
+
+
+n = 1
+output: ["()"]
+
+n = 2
+output: ["(())", "()()"]
+
+n = 3
+output: ["((()))", "(()())", "(())()", "()(())", "()()()"]
+
+n = 4
+output should contain: ["(((())))", "((()()))", "((())())", "((()))()", "(()(()))", "(()()())", "(()())()", "(())(())", "(())()()", "()((()))", "()(()())", "()(())()", "()()(())", "()()()()"]
+ */
